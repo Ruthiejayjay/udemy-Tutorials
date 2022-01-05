056aada0555ce49a17fbf9ffd87628b04eff03a9
jamesruth449 <jamesruth449@gmail.com>
2022-01-05 12:12:41 +0100

    added Dom tutorials

---
+++ script3.js
@@ -0,0 +1,37 @@
+// var button = document.getElementsByTagName("button")[0];
+
+// button.addEventListener("click", function(){
+//     console.log("CLICK!!!");
+// })
+
+
+var button = document.getElementById("enter");
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul");
+
+function inputLenght() {
+   return input.value.length;
+}
+
+function createListItem() {
+   var li = document.createElement("li");
+   li.appendChild(document.createTextNode(input.value));
+   ul.appendChild(li);
+   input.value = "";
+}
+
+function addListAfterClick(){
+
+      if (inputLenght() > 0) {
+         createListItem();
+      }
+}
+
+function addListAfterKeypress(event){
+   if (inputLenght() > 0 && event.keyCode == 13) {
+      createListItem();
+   }
+}
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress)
